e55924dbd67529dcac007d52e9be24e94b07e7b5
Rafael Albuquerque <rafael.p.albuquerque@gmail.com>
2025-09-06 21:25:12 -0300

    refactor: Retirados imports inúteis

--- src/main/java/br/com/fiap/on/iez/IezApplication.java
+++ src/main/java/br/com/fiap/on/iez/IezApplication.java
@@ -5,10 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-import java.io.File;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 @SpringBootApplication
 @EnableScheduling
